6d882b01134a9a7f0577808842cec47bb4572e08
Sidnahar04 <68987629+Sidnahar04@users.noreply.github.com>
2021-09-04 20:54:35 +0530

    Delete facebook/cricket directory

--- facebook/cricket/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-
-def index(request):
-    return render(request,'sample.html')
-    # return HttpResponse("web creyed by sid")
-
-# Create your views here.
