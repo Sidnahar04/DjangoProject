9056cdc53fe633104cbc265c19ccadfe2d6a4acb
Sidnahar04 <68987629+Sidnahar04@users.noreply.github.com>
2021-09-04 20:48:13 +0530

    Add files via upload

---
+++ facebook/friend/admin.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+from .models import Author, Genre, Book, BookInstance
+
+admin.site.register(Book)
+admin.site.register(Author)
+admin.site.register(Genre)
+admin.site.register(BookInstance)
+
+
+
+# Register your models here.
